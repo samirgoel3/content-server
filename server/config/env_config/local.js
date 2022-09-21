145d30f1c9409c28cfd7974d43da35f6078544b7
Samir Goel <samirgoel3@gmail.com>
2022-09-21 13:52:36 +0530

    changes host name

--- server/config/env_config/local.js
+++ server/config/env_config/local.js
@@ -1,6 +1,6 @@
 let localConfig = {
     app:{
-        hostname: "0.0.0.0",
+        hostname: "localhost",
         app_secret:'Algo-Network',
         port:process.env.PORT || 3001,
         base_url: "http://44.206.245.7",
